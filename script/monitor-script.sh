5ef64f3bf1999b3182e1c350c5b9eee6c1c2e3aa
zhiyedan <shijia0620@126.com>
2017-12-06 17:25:29 +0800

    verify the exist of dstat

--- script/monitor-script.sh
+++ script/monitor-script.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 # monitor the cpu/mem/io/net and top-mem/top-cpu
 
-# monitor time(minute)
-min=5
-time_sec=$min*60
-echo 'monitor every '$time_sec' seconde'
+# intervals (minute)
+min=1
 
-folder=/home/zhiyedan/data/monitor
+# folder
+folder=/data/monitor
+
+if hash dstat 2>/dev/null; then
+  echo 'exists dstat'
+else
+  echo 'no exists dstat,exit the process'
+  exit
+fi
+
+time_sec=$[$min*60]
+echo 'monitoring every '$min' minutes'
 
 # cd to the folder
 if [ ! -d "${folder}" ]; then
   mkdir $folder
 fi
-cd $folder 
+cd $folder
 
 # new a file
 host_name=`hostname`
@@ -20,7 +29,6 @@ time=`date "+%Y-%m-%d"`
 file_name=$host_name-$time.csv
 touch $file_name
 
+echo 'the monitor file is '${folder}'/'${file_name}''
 # execute the job
 nohup dstat -tcmnr --top-cpu --top-mem --output $folder/$file_name $time_sec > /dev/null 2>&1 &
-
-echo 'Running successfully'
